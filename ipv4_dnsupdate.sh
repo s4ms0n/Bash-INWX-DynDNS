9279e0c67417d6007d9b9888d16b9ccea2008aa9
Andriy Samsonyuk <andriy.samsonyuk@tngtech.com>
2016-01-14 09:27:49 +0100

    renamed ipv4dnsupdate.sh -> ipv4_dnsupdate.sh

--- ipv4_dnsupdate.sh
+++ ipv4_dnsupdate.sh
@@ -20,7 +20,7 @@ fi
 # update the A-record
 if [ ! "$OLDIPv4" == "$NEWIPv4" ]; then
     echo $NEWIPv4 > ${DATA_FOLDER}/old.ipv4
-    echo "\n\nUpdating IPv4..." >> $LOG_FILE
+    echo "\n"$(date "+%Y-%m-%d %H:%M:%S")": Updating IPv4 $OLDIPv4 -> NEWIPv4\n with response:" >> $LOG_FILE
     DATA=$(cat ${DATA_FOLDER}/nameserver.updateRecord.xml | sed "s/%PASSWD%/$PASSWORD/g;s/%USER%/$USERNAME/g;s/%DNSID%/$DNSIDv4/g;s/%NEWIP%/$NEWIPv4/g")
     curl  -s -X POST -d "$DATA" "$APIHOST" --header "Content-Type:text/xml" >> $LOG_FILE
 fi
